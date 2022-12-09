dad69ef80abca42841a0170a304adc98c4d2d901
Your Name <antony.adriano@stundent.moringaschool.com>
2022-12-09 19:35:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 function hasTargetSum(array, target) {
+  
+    {
+        for (let i = 0; i < array.length; i++) {
+        // n steps (depending on the length of the input array)
+        const complement = target - array[i];
+        for (let j = i + 1; j < array.length; j++) {
+          // n * n steps (nested loop!)
+          if (array[j] === complement) return true;
+        }
+      }
+      // 1 step
+      return false;
+    }
+    
+  }
   // Write your algorithm here
-}
+
 
 /* 
   Write the Big O time complexity of your function here
